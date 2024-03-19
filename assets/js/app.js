628b554666dd8946d0d2fef74dbb7f320cbdb553
leandramusto@gmail.com <leandramusto@gmail.com>
2024-03-19 19:28:22 +0100

    updating ex for in js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,25 @@
-// Create a Vue application instance
-const app = Vue.createApp({
-  // Define the data properties
+const { createApp } = Vue;
+
+createApp({
   data() {
     return {
-      // Array to store the list of emails retrieved from the API
-      emailList: [],
-      // Number of email addresses to retrieve from the API
       numEmail: 10,
-      // URL of the API to fetch random email addresses
+      email: [],
       apiUrl: "https://flynn.boolean.careers/exercises/api/random/mail",
     };
   },
-  // Define methods
-  methods: {
-    // Method to make a GET request to the API to retrieve a random email address
-    getEmail() {
-      return axios.get(this.apiUrl); // Return the axios promise
-    },
-    // Method to fetch multiple email addresses using getEmail method
-    fetchEmails() {
-      // Call getEmail method numEmail times and wait for all promises to resolve
-      Promise.all(Array.from({ length: this.numEmail }, () => this.getEmail()))
-        .then((results) => {
-          // Extract responses from resolved promises and add them to emailList array
-          this.emailList = results.map((result) => result.data.response);
-        })
-        .catch((error) => {
-          // Handle errors if any during fetching of data
-          console.error("Error fetching data:", error);
-        });
-    },
-  },
-  //  Vue instancemounted to the DOM
-  mounted() {
-    // Call fetchEmails method to start fetching email addresses after the instance has been mounted
-    this.fetchEmails();
-  },
-});
 
-// Mount the Vue application instance to the DOM element with id "app"
-app.mount("#app");
+  methods: {
+    getEmail() {
+      axios.get(this.apiUrl).then((result) => {
+        console.log(result.data.response);
+        this.email.push(result.data.response);
+      });
+    },
+  },
+  mounted() {
+    for (let i = 0; i < this.numEmail; i++) {
+      this.getEmail();
+    }
+  },
+}).mount("#app");
